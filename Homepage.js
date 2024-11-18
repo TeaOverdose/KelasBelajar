196310c512c95f96c5cd31ca35adf2bb2b4158e5
MichaelCristiandy <michaelcristiandy9@gmail.com>
2024-11-18 15:11:55 +0700

    Delete Homepage.js

--- Homepage.js
+++
@@ -1,9 +0,0 @@
-const menuOpenButton = document.querySelector("#open-menu");
-const menuCloseButton = document.querySelector("#close-menu");
-
-menuOpenButton.addEventListener("click", () => {
-
-    document.body.classList.toggle("show-mobile-menu");
-});
-
-menuCloseButton.addEventListener("click", () => menuOpenButton.click());
